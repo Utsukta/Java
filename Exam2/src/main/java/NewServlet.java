85154c2be0acbe8cd62b35b5c7d79bb4fd6fe910
Utsukta Khatri <utsukta99kc@gmail.com>
2024-02-04 16:59:05 +0545

    Modified

--- Exam2/src/main/java/NewServlet.java
+++ Exam2/src/main/java/NewServlet.java
@@ -34,6 +34,20 @@ public class NewServlet extends HttpServlet {
         out=response.getWriter();
 
         String name =request.getParameter("email");
+        int no1=Integer.parseInt(request.getParameter("number1"));
+        int no2=Integer.parseInt(request.getParameter("number2"));
+        if(no1>no2){
+            out.print(no1+"is greater");
+            
+            
+        }
+        else if(no2>no1) {
+            out.print(no2+"is greater");
+            
+        }
+        else{
+            out.print("is equal");
+        }
         out.print(name);
        
     }
